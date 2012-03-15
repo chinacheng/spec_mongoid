8608f10e60957f01bee8afd26b74d6449e4d3359
chinacheng <chinachengliwen@gmail.com>
2012-03-15 15:09:47 +0800

    init this project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpecMongoid::Application.config.secret_token = 'adeff7f6c65789163398e67fc5206c320328361a8dc2fa5e26a2c06113fbaedba919706828f43f64f95a0cc14bfc2079e01fae8021b6c9fd46b8ab5047d6dd1c'
