a2e944979ccce4433db72c53ffb2fb20cf9613ce
chinacheng <chinachengliwen@gmail.com>
2013-02-21 17:58:27 +0800

    test user create count

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,12 +3,28 @@ require 'spec_helper'
 describe User do 
 
   before do
-    @user = User.new(:name => "lucy", :login => "lucy", :age => 18)
+    @user = User.new(:name => 'lucy', :login => 'lucy', :age => 18)
   end
 
-  it "descibe lucy" do
-    @user.name.should == "lucy"
+  it 'descibe lucy' do
+    @user.name.should == 'lucy'
     @user.age.should == 18
+  end
+
+  it 'save lucy failure' do
+    begin_count = User.count
+    @user.save
+    end_count = User.count
+    (end_count - begin_count).should be 0 
+  end
+
+  it 'save lucy with the full attributes' do
+    @user.email = 'lucy@tesst.com'
+    @user.password = '123456'
+    begin_count = User.count
+    @user.save
+    end_count = User.count
+    (end_count - begin_count).should be 1 
   end
 
 end
