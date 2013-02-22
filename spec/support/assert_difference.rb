368f7bc13e43419787a265934c1781cdb34797c8
chinacheng <chinachengliwen@gmail.com>
2013-02-22 11:15:23 +0800

    modify the assert difference

--- spec/support/assert_difference.rb
+++ spec/support/assert_difference.rb
@@ -1,12 +1,22 @@
 # coding: utf-8
 
-def assert_difference(execute, number=1, &block)
-  before_size = eval(execute)
+def assert_difference(expression, difference = 1, message = nil, &block)
+  expressions = Array.wrap expression
+
+  exps = expressions.map { |e|
+    e.respond_to?(:call) ? e : lambda { eval(e, block.binding) }
+  }
+  before = exps.map { |e| e.call }
+
   yield
-  end_size = eval(execute)
-  end_size.should == before_size + number
+
+  expressions.zip(exps).each_with_index do |(code, e), i|
+    error  = "#{code.inspect} didn't change by #{difference}"
+    error  = "#{message}.\n#{error}" if message
+    (before[i] + difference).should == e.call
+  end
 end
 
-def assert_no_difference(execute, &block)
-  assert_difference(execute, 0, &block)
+def assert_no_difference(expression, difference = 1, message = nil, &block)
+  assert_difference expression, 0, message, &block
 end
